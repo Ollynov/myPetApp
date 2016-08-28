f0fc82471ca72c8e5e4c40c49d98430c4ad44df3
Aljosha Novakovic <ollynov@gmail.com>
2016-08-28 16:41:25 -0700

    new files added

---
+++ process/js/app.js
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var MainInterface = React.createClass({
+	render: function() {
+		return <h1>Pet Clinic Appointments</h1>
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<MainInterface />, document.getElementById('petAppointments'));
