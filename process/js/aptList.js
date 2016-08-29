a5e918d0da76342558e77b37311195bf64e967fc
Aljosha Novakovic <ollynov@gmail.com>
2016-08-28 23:23:25 -0700

    added styles and bookstrap

--- process/js/aptList.js
+++ process/js/aptList.js
@@ -4,6 +4,10 @@ var AptList = React.createClass({
 	render: function() {
     return(
       <li>
+        <div className="media-left">
+          <button className="pet-delete btn btn-xs btn-danger" onClick={ this.handleDelete }>
+            <span className="glyphicon glyphicon-remove"></span>
+          </button>
         <div>
           <span>{ this.props.singleItem.petName } </span>
           Appointment on: <span>{ this.props.singleItem.aptDate }</span>
